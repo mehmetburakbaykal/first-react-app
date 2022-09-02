bfd747d2fb16f5b0520acad525a5fb8246eedeed
mehmetburakbaykal <mehmetburakbaykal1@gmail.com>
2022-09-03 02:43:24 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
-import React from "react";
-import Counter from "./Counter";
+import React, { useState } from "react";
+import "./style.css";
 
 function App() {
-  return <Counter />;
+  const [count, setCount] = useState(0);
+  function CounterIncrease() {
+    return setCount(count + 1);
+  }
+  function CounterDecrease() {
+    return setCount(count - 1);
+  }
+  function CounterReset() {
+    return setCount(0);
+  }
+  return (
+    <div id="container">
+      <h1 className="center-text">COUNTER</h1>
+      <h2 className="center-text">{count}</h2>
+      <button
+        className="btn btn-inc"
+        onClick={CounterIncrease}
+        disabled={count === 10}
+      >
+        Increase
+      </button>
+      <button
+        className="btn btn-dec"
+        onClick={CounterDecrease}
+        disabled={count === 0}
+      >
+        Decrease
+      </button>
+      <button className="btn btn-reset" onClick={CounterReset}>
+        Reset
+      </button>
+    </div>
+  );
 }
 
 export default App;
